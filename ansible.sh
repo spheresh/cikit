9b6f0ada0996eb906d4d63bf224fc7a8a0766fca
Sergii Bondarenko <broken@propeople.com.ua>
2015-11-12 11:06:49 +0200

    Added possibility to run playbooks from a directory and look for available task for ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -11,7 +11,7 @@
 # - eval
 # - ansible-playbook
 
-# List of variables which will be passed as parameters for "ansible-playbook".
+# List of allowed variables for passing as parameters for "ansible-playbook".
 APB_VARS="limit tags list-tags module-path"
 
 locate_path()
@@ -29,24 +29,46 @@ locate_path()
     echo ${SELF_PATH}
 }
 
-SELF_PATH=$(locate_path ${0})
-SCRIPTS_PATH=${SELF_PATH}/scripts
-PLAYBOOK=${1%%.*}.yml
-
-for playbook in ${SCRIPTS_PATH}/${PLAYBOOK} ${SELF_PATH}/${PLAYBOOK} ${PLAYBOOK}; do
-    if [ -f "${playbook}" ]; then
-        cd $(locate_path ${playbook})
-        break
-    fi
-done
-
-if [ -z ${1} ]; then
-    cd ${SCRIPTS_PATH}
+exit_with_help()
+{
     echo "Available operations:"
     for operation in $(ls -A *.yml); do
         echo "- ${operation%%.*}"
     done
     exit 0
+}
+
+SELF_PATH=$(locate_path ${0})
+SCRIPTS_PATH=${SELF_PATH}/scripts
+
+if [ -z ${1} ]; then
+    cd ${SCRIPTS_PATH}
+    exit_with_help
+fi
+
+POSSIBLE_SELF_PATH=${1}/empty
+locate_path ${POSSIBLE_SELF_PATH} > /dev/null 2>&1
+
+if [ $? -gt 0 ]; then
+    POSSIBLE_SELF_PATH=$(locate_path ${POSSIBLE_SELF_PATH})
+fi
+
+if [ -d ${POSSIBLE_SELF_PATH} ]; then
+    cd ${POSSIBLE_SELF_PATH}
+    exit_with_help
+fi
+
+PLAYBOOK=${1%%.*}.yml
+
+for playbook in ${SCRIPTS_PATH}/${PLAYBOOK} ${SELF_PATH}/${PLAYBOOK} ${PLAYBOOK}; do
+    if [ -f ${playbook} ]; then
+        break
+    fi
+done
+
+if [ ! -f ${playbook} ]; then
+    cd ${SCRIPTS_PATH}
+    exit_with_help
 fi
 
 extra_vars=""
