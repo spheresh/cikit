b8e90c80726be248d348d3936f5121ba244fd591
Sergii Bondarenko <broken@propeople.com.ua>
2015-11-17 11:09:26 +0200

    Added ignoring paths to Vagrantfile

--- files/fakeswap/fakeswap.sh
+++ files/fakeswap/fakeswap.sh
@@ -6,13 +6,11 @@
 # for adding 4086MB swapsize
 
 SWAP="${1:-512}"
-
-NEW="$[SWAP*1024]"; TEMP="${NEW//?/ }"; OLD="${TEMP:1}0"
+NEW="$[SWAP*1024]"
+TEMP="${NEW//?/ }"
+OLD="${TEMP:1}0"
 
 umount /proc/meminfo 2> /dev/null
-
 sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
-
 mount --bind /etc/fake_meminfo /proc/meminfo
-
 free -m
