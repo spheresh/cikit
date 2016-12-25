c99d7907da324fab8042442057914aab8687a583
Sergii Bondarenko <broken@firstvector.org>
2016-12-25 22:14:49 +0400

    Allow to run "selenium.sh" without downloading URL and allow to overwrite the package

--- cmf/all/selenium.sh
+++ cmf/all/selenium.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
+# Download and (re)start:
+# ./selenium.sh http://example.com/selenium/2.46.jar
+# Re-download and (re)start:
+# ./selenium.sh http://example.com/selenium/2.53.jar --force
+# Restart:
+# ./selenium.sh
+
 DOWNLOAD_URL="$1"
 FILENAME="selenium.jar"
 ROOT=".vagrant"
-
-if [[ -z ${DOWNLOAD_URL} || ! -d ${ROOT} ]]; then
-  echo "Download URL or ${ROOT} directory does not exists."
-  exit 1
-fi
 
 DESTINATION="${ROOT}/${FILENAME}"
 LOGFILE="${ROOT}/${FILENAME%%.*}"
 PIDFILE="${LOGFILE}.pid"
 
-if [ ! -f ${DESTINATION} ]; then
+if [[ ! -f ${DESTINATION} || "$2" == "--force" ]]; then
+  if [ -z ${DOWNLOAD_URL} ]; then
+    echo "You have not specified an URL for downloading."
+    exit 1
+  fi
+
+  if [ ! -d ${ROOT} ]; then
+    echo "Virtual machine is not running. Try \"vagrant up\" to fix this."
+    exit 2
+  fi
+
   curl -O ${DOWNLOAD_URL}
 
   if [ $? -gt 0 ]; then
     echo "Cannot download file: ${DOWNLOAD_URL}"
-    exit 1
+    exit 3
   fi
 
   mv ${DOWNLOAD_URL##*/} ${DESTINATION}
